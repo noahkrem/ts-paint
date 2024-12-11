c1be9a26d9010013e4f8b50651ed4078f79f3fe7
Noah Kremler <kremlern@gmail.com>
2024-12-10 20:51:17 -0800

    Compute Point function completed

--- hooks/useDraw.tsx
+++ hooks/useDraw.tsx
@@ -1,18 +1,37 @@
 import { useEffect, useRef } from "react"
 
-export const useDraw = () => {
+export const useDraw = (onDraw: ({ctx, currentPoint, prevPoint}: Draw)) => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
 
-        const handler = (e: MouseEvent) => {
+        const handler = (e: MouseEvent  ) => {
+            const currentPoint = computePointInCanvas(e)
+            console.log(currentPoint?.x, currentPoint?.y)
+
+            // Context of the canvas
+            const ctx = canvasRef.current?.getContext('2d')
+            if (!ctx || !currentPoint) return   // If there is no context or current point return
 
         }
 
+        const computePointInCanvas = (e: MouseEvent) => {
+            const canvas = canvasRef.current 
+            if (!canvas) return // If canvas is Null, return
+
+            const rect = canvas.getBoundingClientRect()
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top
+
+            return { x, y }
+        }
+
         // Add event listeners
-        canvasRef.current?.addEventListener('mousemove', e => handler)
-        
+        canvasRef.current?.addEventListener('mousemove', handler)
+
+        // Remove event listeners
+        return () => canvasRef.current?.addEventListener('mousemove', handler)
     }, [])
 
     return  { canvasRef }
---
+++ types/typing.d.ts
@@ -0,0 +1,2 @@
+// A *.d.ts file contains only type annotations, and is called a 'Typescript Definition' file
+
