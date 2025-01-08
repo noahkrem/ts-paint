dfcd61482113d1b2b6fffd4f8289ae706f8e8583
Noah Kremler <kremlern@gmail.com>
2025-01-07 20:39:34 -0800

    tsconfig added, server now listening on 3001

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
+import { Server } from 'socket.io'
+
 const express = require('express')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
-
-import { Server } from 'socket.io'
 
 const io = new Server(server, {
     cors: {
